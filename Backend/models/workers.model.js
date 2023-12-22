924a483eb71d7d98364fb78ddb2407c9eb394068
Efrain06 <ev140311@gmail.com>
2023-12-21 21:17:46 -0500

    UPDATE

---
+++ Backend/models/workers.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkersSchema = Schema({
+    name:{type: String, required: true, max:60}
+    name:{type: String, required: true, max:60}
+    name:{type: String, required: true, max:60}
+    name:{type: String, required: true, max:60}
+    name:{type: String, required: true, max:60}
+})
--- README.md
+++
@@ -1,1 +0,0 @@
-# App_Web_Gestion
